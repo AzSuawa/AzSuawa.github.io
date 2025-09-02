597d851ff6ded6441c954d0651d78ec850ed52af
AzSu <asubensu@outlook.com>
2025-09-02 22:28:53 +0800

    更新 main.js

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@ const pageContentMap = {
     'g': 'pages/g.html',
     'mcp': 'mcp.html',
     'skin': 'skim.html',
+    'api': 'api.html',
     'sp': 'pages/sp.html',
     'ban-qwqwcllwww': 'pages/ban/qwqwcllwww.html',
     'ban-mam1145': 'pages/ban/mam1145.html',
